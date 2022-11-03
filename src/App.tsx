72c56bab3be70237630c9701c9a7b5c2cec7e125
Gihun <563710@naver.com>
2022-11-03 18:05:06 +0900

    feature(App): 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "@/home/HomePage";
+import Project from "@/project/Project";
+import LeftNavibar from "@/common/navibar/LeftNavibar";
+import InfoPage from "./info/InfoPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div></div>} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        <Route path="/" element={<LeftNavibar element={<HomePage />} />} />
+        <Route path="/info" element={<LeftNavibar element={<InfoPage />} />} />
+        <Route
+          path="/project"
+          element={<LeftNavibar element={<Project />} />}
+        />
+        <Route path="*" element={<Navigate replace to="/" />} />{" "}
       </Routes>
     </BrowserRouter>
   );
